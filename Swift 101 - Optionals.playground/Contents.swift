8927b34c60124714be6c8e4483637a5512090b5b
Adam Gask <ajgask@gmail.com>
2017-05-29 18:27:15 +0100

    Added Playground

---
+++ Swift 101 - Optionals.playground/Contents.swift
@@ -0,0 +1,15 @@
+/*:
+ # Swift 101 - nil
+ XXXXX
+ 
+ Today I’ll be blogging about another fundamental concept in Swift — Optional variables.
+ */
+
+// The following variables are all optional
+
+var optionalInt: Int?
+var optionalBool: Bool?
+var optionalString: String?
+
+// Uncomment the next line to how optional variables must not be declared `let`
+//let optionalLetString: String?
